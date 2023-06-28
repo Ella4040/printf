3f5b7b7ed42739d18351fd600b7f278c6f109d03
khaoula1972 <khaoula.nakkach@gmail.com>
2023-06-28 15:15:21 -0700

    our putchar function

---
+++ putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - Writes a character to the standard output stream
+ * @b: The character to be printed.
+ * Return: 1 on success, printed character, - if failures occure
+ */
+int _putchar(char  c)
+{
+	return (write(1, &c, 1));
+}
