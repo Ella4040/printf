e77391e3575fcb730032f7836f78c406aeca4730
khaoula1972 <khaoula.nakkach@gmail.com>
2023-06-29 07:51:51 -0700

    our customized printf

--- _printf.c
+++ _printf.c
@@ -6,4 +6,49 @@
  */
 int _printf(const char *format, ...)
 {
-	int chars;
+	va_list args; /* a variadic function */
+	int count = 0;
+	convert_choice list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_decimal},
+		{"i", print_decimal},
+		{NULL, NULL}
+	};
+	convert_choice *ptr = list;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%') /* To see if there's any specifier */
+		{
+			format++; /*in this case, we'll move to the next character*/
+
+			if (ptr->specifier == NULL)
+			{
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+			}
+			
+			while (ptr->specifier != NULL)
+			{
+			if (*format == *(ptr->specifier))
+			{
+				count += ptr->function(args); /* we call the matching function */
+				break;
+			}
+			ptr++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++; /* let's move to the next character */
+	}
+	va_end(args); /* to clean up the list of arguments */
+	return (count); /* The count of printed characters */
+}
