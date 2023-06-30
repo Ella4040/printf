0faec39f7786ba6b09a7237241481127e285fd79
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-06-30 21:54:02 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _printf - a function that produces output according to a format.
- * @format: The format string specifying the desired output format
- * Return: the number of characters printed
+ * _printf - a function that produces output according to a format
+ * @format: The format string specifying the desired output format.
+ * Return: the number of characters printed.
  */
 int _printf(const char *format, ...)
 {
