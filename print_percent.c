accc6d211dc38a13bccf64d6568367b2a3fcc80e
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-06-30 21:46:26 +0100

    Update print_percent.c

--- print_percent.c
+++ print_percent.c
@@ -2,7 +2,7 @@
 /**
  * print_percent - A function that print the symbol %
  * @args: a list containing the character to be printed.
- * Return: the number of characters printed.
+ * Return: the number of characters  printed.
  */
 int print_percent(va_list args)
 {
