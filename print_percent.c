c23325c9b9604d58ddd15dee6fc1807bec29e1d5
Khaoula Nakkach <123691690+khaoula1972@users.noreply.github.com>
2023-06-30 02:12:20 +0100

    print percent symbol

--- print_percent.c
+++ print_percent.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_percent - A function that print the symbol %
- * @args: a list containing the character to be printed
+ * @args: a list containing the character to be printed.
  * Return: the number of characters printed.
  */
 int print_percent(va_list args)
