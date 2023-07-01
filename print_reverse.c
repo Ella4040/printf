ee0ea545f20559ebd6ddd1e61ec7174ba229e0d6
khaoula1972 <khaoula.nakkach@gmail.com>
2023-07-01 02:17:37 +0100

    print in reverse

---
+++ print_reverse.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_reverse - A function to print a string in reverse
+ * @args: a list containing the characters to print
+ * Return:  the number of characters printed, -1 if error occures
+ */
+int print_reverse(va_list args)
+{
+	char *list = va_arg(args, char *); /* Get the character list from the variable argument list */
+	int length; /* the variable where we're gonna store the string's length */
+	int j; /* for the loop we used */
+
+	if (list == NULL)
+	{
+		list = "(null)";
+	}
+	length = len(list); /* To go through the string */
+	for (j = length - 1; j >= 0; j--)
+	{		
+		_putchar(list[j]);
+	}
+	return (length);
+}
